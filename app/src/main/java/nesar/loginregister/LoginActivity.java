7c50c7712b5491834dd0bfc1efa325e2bbbd6e1d
Nesar13 <nahmed3@stevens.edu>
2017-01-04 01:36:12 -0500

    Exporting

---
+++ app/src/main/java/nesar/loginregister/LoginActivity.java
@@ -0,0 +1,23 @@
+package nesar.loginregister;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void sendMessage(View v) {
+
+
+        Intent intent = new Intent(this, RegisterActivity.class);
+        startActivity(intent);
+    }
+}
